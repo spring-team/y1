a7dbd5b9334cf17e70729fec1c85153ebeb8c26c
Rod Johnson <johnsonroda@gmail.com>
2018-03-14 20:56:07 +1100

    Initial commit from Atomist

---
+++ src/main/java/c/Y1Application.java
@@ -0,0 +1,12 @@
+package c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Y1Application.class, args);
+	}
+}
---
+++ src/test/java/c/Y1ApplicationTests.java
@@ -0,0 +1,16 @@
+package c;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Y1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
